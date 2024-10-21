b24ded92e47c95813835ff500cda1c94a14251ed
TamiBeira <tamirisbeira@gmail.com>
2024-10-21 14:02:57 -0300

    add propiedades de login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import { ChannelContextProvider } from "./context/ChannelContext";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <ChannelContextProvider>
+            <App />
+        </ChannelContextProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
--- frontend/src/screens/Login.tsx
+++ frontend/src/screens/Login.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import { useChannelContext } from "../hooks/useChannelContext";
 
 export const Login = () => {
     const [userName, setUserName] = useState("");
+    const { login } = useChannelContext();
     return (
         <div>
             <h1>Login</h1>
             <form
                 onSubmit={(event) => {
                     event.preventDefault();
+                    login(userName);
                 }}
             >
                 <label>Username</label>
