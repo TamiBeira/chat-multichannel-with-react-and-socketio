e062b77761a6bb05194065e1a0826125d8cf6bad
TamiBeira <tamirisbeira@gmail.com>
2024-10-21 16:24:24 -0300

    implementando chat

--- frontend/src/screens/ChannelCreate.tsx
+++ frontend/src/screens/ChannelCreate.tsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useChannelContext } from "../hooks/useChannelContext";
+import { useNavigate } from "react-router-dom";
 
 export const ChannelCreate = () => {
     const [channelName, setChannelName] = useState("");
-    const { createChannel } = useChannelContext();
+    const { createChannel, userName } = useChannelContext();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!userName) {
+            navigate("/login");
+            return;
+        }
+    }, []);
 
     return (
         <div>
--- frontend/src/screens/Chat.tsx
+++ frontend/src/screens/Chat.tsx
@@ -1,35 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useChannelContext } from "../hooks/useChannelContext";
-import { Link } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 
 export const Chat = () => {
     const [message, setMessage] = useState("");
-    const { channel } = useChannelContext();
+    const { channel, joinChannel, createMessage, userName } =
+        useChannelContext();
+    const { channelId } = useParams<{ channelId: string }>();
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        if (!channelId) {
+            navigate("/channels");
+            return;
+        }
+        joinChannel(channelId);
+    }, []);
+
+    if (!userName) {
+        navigate("/login");
+        return;
+    }
     return (
-        <div>
+        <>
             <div>
-                <h5># {channel?.name}</h5>
-                <Link to="/channels">Voltar</Link>
-            </div>
-            <div>
-                {channel?.messages.map((message) => (
-                    <div>
-                        <strong>{message?.userName}</strong>: {message?.message}
-                    </div>
-                ))}
-            </div>
-            <form>
-                <input
-                    placeholder="Escreva sua mensagem"
-                    type="text"
-                    value={message}
-                    onChange={(ev) => {
-                        setMessage(ev.target.value);
+                <div>
+                    <h5># {channel?.name}</h5>
+                    <Link to="/channels">Voltar</Link>
+                </div>
+                <div>
+                    {channel?.messages.map((message, index) => (
+                        <div key={index}>
+                            <strong>{message?.userName}</strong>:{" "}
+                            {message?.message}
+                        </div>
+                    ))}
+                </div>
+                <form
+                    onSubmit={(event) => {
+                        event.preventDefault();
+                        createMessage(message);
+                        setMessage("");
                     }}
-                />
-                <button>Enviar</button>
-            </form>
-        </div>
+                >
+                    <input
+                        placeholder="Escreva sua mensagem"
+                        type="text"
+                        value={message}
+                        onChange={(ev) => {
+                            setMessage(ev.target.value);
+                        }}
+                    />
+                    <button>Enviar</button>
+                </form>
+            </div>
+        </>
     );
 };
